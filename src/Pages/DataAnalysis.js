83e8eb6e0ac93b2acd4e11e7ddeb37a337ad0c4b
Baxx Angelo <90490236+BazzSHOCKCoder@users.noreply.github.com>
2023-04-16 01:22:52 +0800

    Data Analysis update

--- src/Pages/DataAnalysis.js
+++ src/Pages/DataAnalysis.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React from 'react';
+import './DataAnalysis.css';
 
 function DataAnalysis() {
-  return <div>DataAnalysis</div>;
+  return (
+    <div className="main">
+      <header>
+        <h1> Data Analysis </h1>
+      </header>
+      <center>
+        <div className="chart-container">
+          <div className="chart">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+          <br />
+          <br />
+          <div className="report-chart">
+            <div className="chart-container">
+              <div className="chart-area">
+                <br />
+                <input type="button" className="anybutton" id="myab" value="Create Chart" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </center>
+    </div>
+  );
 }
 
 export default DataAnalysis;
